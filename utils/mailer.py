d73d1e52445f7c2c80062e1acc32fe5200c4b726
YOU-JIE <h125872359@gmail.com>
2025-08-14 11:14:51 +0800

    repo: cleanup & standardize (namespace+shims+lint+tests+build)

--- utils/mailer.py
+++ utils/mailer.py
@@ -1,2 +1,2 @@
-import smtplib  # keep patchable: tests use patch("utils.mailer.smtplib.SMTP_SSL")
+import smtplib  # noqa: F401  # keep patchable: tests use patch("utils.mailer.smtplib.SMTP_SSL")
 from smart_mail_agent.utils.mailer import *  # noqa: F401,F403
